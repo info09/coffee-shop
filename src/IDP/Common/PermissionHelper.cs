4070663c2bdd42d7db2483182bee623da188bead
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-04-09 14:19:57 +0700

    Add permission to claims

---
+++ src/IDP/Common/PermissionHelper.cs
@@ -0,0 +1,33 @@
+﻿using IDP.Infrastructure.Common;
+using IDP.Infrastructure.ViewModels;
+
+namespace IDP.Common;
+
+public static class PermissionHelper
+{
+    public static string GetPermission(string function, string command)
+    {
+        return $"{function}.{command}";
+    }
+
+    public static List<PermissionAddModel> GetAllPermissions()
+    {
+        var permissions = new List<PermissionAddModel>();
+        var functions = SystemConstants.Functions.GetAllFunctions();
+        var commands = SystemConstants.Permissions.GetAllCommands();
+
+        foreach (var function in functions)
+        {
+            foreach (var command in commands)
+            {
+                permissions.Add(new PermissionAddModel
+                {
+                    Function = function,
+                    Command = command
+                });
+            }
+        }
+
+        return permissions;
+    }
+}
--- src/IDP/Extensions/IdentityProfileService.cs
+++ src/IDP/Extensions/IdentityProfileService.cs
@@ -5,6 +5,9 @@ using IDP.Infrastructure.Entities;
 using IDP.Infrastructure.Common;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using IDP.Infrastructure.Repositories;
+using IDP.Common;
+using System.Text.Json;
 
 namespace IDP.Extensions;
 
@@ -12,11 +15,13 @@ public class IdentityProfileService : IProfileService
 {
     private readonly IUserClaimsPrincipalFactory<User> _claimsFactory;
     private readonly UserManager<User> _userManager;
+    private readonly IRepositoryManager _repositoryManager;
 
-    public IdentityProfileService(IUserClaimsPrincipalFactory<User> claimsFactory, UserManager<User> userManager)
+    public IdentityProfileService(IUserClaimsPrincipalFactory<User> claimsFactory, UserManager<User> userManager, IRepositoryManager repositoryManager)
     {
         _claimsFactory = claimsFactory;
         _userManager = userManager;
+        _repositoryManager = repositoryManager;
     }
     public async Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
@@ -30,8 +35,8 @@ public class IdentityProfileService : IProfileService
         var principal = await _claimsFactory.CreateAsync(user);
         var claims = principal.Claims.ToList();
         var roles = await _userManager.GetRolesAsync(user);
-        //var permissionQuery = await _repositoryManager.Permission.GetPermissionsByUser(user);
-        //var permissions = permissionQuery.Select(x => PermissionHelper.GetPermission(x.Function, x.Command));
+        var permissionQuery = await _repositoryManager.Permission.GetPermissionsByUser(user);
+        var permissions = permissionQuery.Select(x => PermissionHelper.GetPermission(x.Function, x.Command));
         //Add more claims like this
         claims.Add(new Claim(SystemConstants.Claims.FirstName, user.FirstName));
         claims.Add(new Claim(SystemConstants.Claims.LastName, user.LastName));
@@ -41,7 +46,7 @@ public class IdentityProfileService : IProfileService
         claims.Add(new Claim(ClaimTypes.Email, user.Email!));
         claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
         claims.Add(new Claim(SystemConstants.Claims.Roles, string.Join(";", roles)));
-        //claims.Add(new Claim(SystemConstants.Claims.Permissions, JsonSerializer.Serialize(permissions)));
+        claims.Add(new Claim(SystemConstants.Claims.Permissions, JsonSerializer.Serialize(permissions)));
 
         context.IssuedClaims = claims;
     }
